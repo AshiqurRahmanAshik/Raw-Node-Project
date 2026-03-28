9142f86359f7a08cd9fad84e10689249e67802e3
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-03-28 11:52:53 +0600

    Path: Path Setup for Handling Request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // dependencies
 const http = require("http");
+const { parse } = require("path");
+const url = require("url"); // this dependencies is required to work with path
 
 // app object
 const app = {};
@@ -19,6 +21,15 @@ app.createServer = () => {
 
 // handle request and response
 app.handleReqRes = (req, res) => {
+  // Request Handling
+  //const parsedUrl = url.parse(req.url, true); //(Older Method)
+  const parsedUrl = new URL(req.url, `http://${req.headers.host}`); // New Method
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  console.log(trimmedPath);
+
+  // Response Handling
   res.end("Hello World");
 };
 
