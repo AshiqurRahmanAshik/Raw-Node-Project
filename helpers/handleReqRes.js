2ec282826b7b32ae58c31a50b79dbab02faa05d9
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2026-03-29 11:06:58 +0600

    Implement Routing

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -2,6 +2,11 @@ const url = require("url");
 const path = require("path");
 const { StringDecoder } = require("string_decoder");
 
+const routes = require("../routes");
+const {
+  notFoundHandler,
+} = require("../handlers/routeHandlers/notFoundHandlers");
+
 const handler = {};
 
 handler.handleReqRes = (req, res) => {
@@ -12,8 +17,27 @@ handler.handleReqRes = (req, res) => {
   const queryStringObject = Object.fromEntries(parsedUrl.searchParams);
   const headers = req.headers;
 
+  const requestProperties = {
+    parsedUrl,
+    trimmedPath,
+    method,
+    queryStringObject,
+    headers,
+  };
   const decoder = new StringDecoder("utf-8");
   let realData = "";
+
+  const chosenHandler = routes[trimmedPath]
+    ? routes[trimmedPath]
+    : notFoundHandler;
+
+  chosenHandler(requestProperties, (statusCode, payload) => {
+    statusCode = typeof statusCode === "number" ? statusCode : 500;
+    payload = typeof payload === "object" ? payload : {};
+    const payLoadString = JSON.stringify(payload);
+    res.writeHead(statusCode);
+    res.end(payLoadString);
+  });
 
   req.on("data", (buffer) => {
     realData += decoder.write(buffer);
